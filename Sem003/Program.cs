70b0bcbefbd03ae95f60f74982bd2da39c80d172
Morrigan <stepanyancarine@yandex.ru>
2022-08-20 13:18:28 +0300

    Задача от минуса до плюса

---
+++ Sem003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int x= Convert.ToInt32(Console.ReadLine());
+int y=0-x;
+while(y<=x){
+    if(y<x){
+    Console.Write(y+",");
+    y++;}
+    if (y==x)
+    {Console.Write(y);
+    break;}
+
+}
