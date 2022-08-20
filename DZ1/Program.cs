a4f5f5b7076f6a8c23d4d759b97e14410e912bf1
Morrigan <stepanyancarine@yandex.ru>
2022-08-20 07:29:08 +0300

    Домашнее задание задача 2

---
+++ DZ1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число: ");
+int x=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int y=Convert.ToInt32(Console.ReadLine());
+if (x>y){
+Console.WriteLine("max="+ x+ " ,min="+y);    
+}
+else{
+Console.WriteLine("max="+ y+ " ,min="+x);
+};
