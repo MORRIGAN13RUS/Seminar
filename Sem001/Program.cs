9e997d1b2a316eed545ff7f9b931fe05912591f8
Morrigan <stepanyancarine@yandex.ru>
2022-08-20 11:57:16 +0300

    Программа Квадратный корень проверка

--- Sem001/Program.cs
+++ Sem001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите первое число: ");
+int x= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int y=Convert.ToInt32(Console.ReadLine());
+if (y*y==x){
+    Console.WriteLine(y+" является квадратным корнем из "+x);
+}
+else{
+    Console.WriteLine(y+"  не является квадратным коренем из " + x);
+}
