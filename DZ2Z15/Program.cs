d1e1782b7f7aae77c049c8111c6b828edbfceaed
Morrigan <stepanyancarine@yandex.ru>
2022-08-22 15:32:24 +0300

    Определение выходного дня по вводимому числу+метод

--- DZ2Z15/Program.cs
+++ DZ2Z15/Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.WriteLine("Введите номер дня недели");
 int x=Convert.ToInt32(Console.ReadLine());
+void Weekend (int x) {
 if(x>5) Console.WriteLine("Ура! Бухаем!");
 else Console.WriteLine("Работайте, черти!");
+}
+Weekend (x);
