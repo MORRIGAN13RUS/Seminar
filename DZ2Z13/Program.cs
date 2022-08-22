ea8b5989ee34be08f2d2704b58ba38973e28aa83
Morrigan <stepanyancarine@yandex.ru>
2022-08-22 15:10:18 +0300

    Вывод третьего  элемента из трехзначного числа

---
+++ DZ2Z13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое число: ");
+char [] arr=Console.ReadLine().ToCharArray();
+if(arr[0]=='-'){
+    if(arr.Length<4)
+    Console.WriteLine("Третьего числа нет");
+    else Console.WriteLine(arr[3]);
+}
+else{
+    if(arr.Length<3)
+    Console.WriteLine("Третьего числа нет");
+    else
+    Console.WriteLine(arr[2]);
+}
