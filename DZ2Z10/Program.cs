f4077e13fd14427de2e98416ab7fb6eed3984388
Morrigan <stepanyancarine@yandex.ru>
2022-08-22 14:35:00 +0300

    Вывод второго эелемента изз трехзначного числа

---
+++ DZ2Z10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+char [] arr=Console.ReadLine().ToCharArray();
+if(arr[0]=='-'){
+    Console.WriteLine(arr[3]);
+}
+else{
+    Console.WriteLine(arr[2]);
+}
