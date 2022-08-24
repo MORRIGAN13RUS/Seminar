40423fdec4c9dbb3026eab5a2752ecd2e4a271d9
Morrigan <stepanyancarine@yandex.ru>
2022-08-24 15:58:26 +0300

    Первыйй вариант решения задачи номер 19поправка проверки на минус

--- DZ3Z19/Program.cs
+++ DZ3Z19/Program.cs
@@ -4,7 +4,11 @@ Console.WriteLine("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 bool CheckPal(int x)
 {
-    if (x < 0) { x = x * -1; }
+    bool check = false;
+    if (x < 0)
+    {
+        return check;
+    }
     int a5 = x % 10;
     x = x / 10;
     int a4 = x % 10;
@@ -13,7 +17,6 @@ bool CheckPal(int x)
     x = x / 10;
     int a2 = x % 10;
     int a1 = x / 10;
-    bool check = false;
     if (a5 == a1 && a4 == a2)
     {
         check = true;
