36628aa86cb72f13cb639645e1f9d31123b3e5f5
Morrigan <stepanyancarine@yandex.ru>
2022-08-24 15:54:23 +0300

    Третий вариант решения задачи номер 19

---
+++ DZ3Z19c/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//Третий способ  через массив char, универсально для любого количества символов в числе, но не более 10 (для int)
+Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool CheckPal(int x)
+{
+    bool check = true;
+    if (x < 0)
+    {
+        check = false;
+        return check;
+    }
+    char[] arr = x.ToString().ToCharArray();
+    for (int i = 0; i < (arr.Length - 1) / 2; i++)
+    {
+        if (arr[i] != arr[arr.Length - 1 - i])
+        {
+            check = false;
+            break;
+        }
+    }
+    return check;
+}
+Console.WriteLine(CheckPal(num));
