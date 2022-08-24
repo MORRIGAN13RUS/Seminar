106f4ee43722e95400c2fc6753ed1fb34baf2eba
Morrigan <stepanyancarine@yandex.ru>
2022-08-24 16:38:57 +0300

    Решение задачи 21

---
+++ DZ3Z21/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+double[] A = { 3, 6, 8 };
+double[] B = { 2, 1, -7 };
+double GetCad(double[] arr1, double[] arr2)
+{
+    double cad = Math.Sqrt(
+        Math.Pow(arr2[0] - arr1[0], 2)
+        + Math.Pow(arr2[1] - arr1[1], 2)
+        + Math.Pow(arr2[2] - arr1[2], 2));
+    return cad;
+}
+Console.WriteLine("Длинна отрезка:" + GetCad(A, B));
