d04a59ca77f6776e8afa0ef1368c5be1ae808327
Morrigan <stepanyancarine@yandex.ru>
2022-08-26 23:32:20 +0300

    Задача 21, усложнили ввод координат

--- DZ3Z21/Program.cs
+++ DZ3Z21/Program.cs
@@ -1,7 +1,24 @@
 ﻿//Напишите программу, которая принимает на вход координаты 
 //двух точек и находит расстояние между ними в 3D пространстве.
-double[] A = { 3, 6, 8 };
-double[] B = { 2, 1, -7 };
+double[] A = new double [3];
+double[] B = new double [3];
+GetArr(A);
+GetArr(B);
+double [] GetArr(double [] Arr){
+     Console.WriteLine("Введите коорднаты точки: ");
+    for(int i=0; i<3; i++ ){       
+        bool isNumber=int.TryParse(Console.ReadLine(),out int x);
+        if (isNumber){
+Arr[i]=x;
+        }
+        else {
+            Console.WriteLine("Координаты введены неверно.Повторите ввод сначала!");
+        GetArr(Arr);
+        }
+
+    }
+    return Arr;
+}
 double GetCad(double[] arr1, double[] arr2)
 {
     double cad = Math.Sqrt(
